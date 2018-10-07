9787269b24278075298ba8bb90688e2161843b49
Benjamin Caradeuc <bencrdc@gmail.com>
2018-10-07 17:48:33 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app-wrapper">
+    <app-header></app-header>
+    <app-sidebar></app-sidebar>
+    <main>
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader'
+import AppSidebar from './components/AppSidebar'
+
+export default {
+  name: "App",
+  components: { AppHeader, AppSidebar }
+}
+</script>
+
+<style lang="scss">
+html {
+  font-size: 100%;
+}
+body {
+  font-family: sans-serif;
+  color: pink;
+}
+[v-cloak] {
+  display: none;
+}
+</style>
